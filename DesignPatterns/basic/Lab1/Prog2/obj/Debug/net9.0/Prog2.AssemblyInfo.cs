fbf058ef3dbc9b5be4f736cd98c509c7ff8ab1e4
Oxana <godorozaoksana92@gmail.com>
2025-10-19 21:38:06 +0300

    CloudComputing3lab

--- DesignPatterns/basic/Lab1/Prog2/obj/Debug/net9.0/Prog2.AssemblyInfo.cs
+++ DesignPatterns/basic/Lab1/Prog2/obj/Debug/net9.0/Prog2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prog2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7a3fc0a486a07292aca3778e0363e225a66659c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90421a1973069a7c4f0d4c06651bdd18cacf9303")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prog2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prog2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
